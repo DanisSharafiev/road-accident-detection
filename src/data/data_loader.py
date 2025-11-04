905f3ebd91979b5d85eae3499c2629f7be37c462
ddsharafiev <ddsharafiev@avito.ru>
2025-11-04 11:34:41 +0300

    feature: add randomcrop

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -31,15 +31,13 @@ class CustomDataset(Dataset):
             image = self.transform(image)
 
         label = self.labels[idx]
-
         return image, label
 
 def get_data_loader(image_dict : dict[str, int], 
                     batch_size : int = 32, 
                     shuffle : bool = True, 
-                    num_workers : int = 4, 
-                    augment : bool = False,
-                    custom_transforms: transforms.Compose | None = None
+                    num_workers : int = 4,
+                    augment: transforms.Compose | bool = False
                     ) -> DataLoader:
     """returns a pytorch DataLoader for the given image dictionary.\n
     image_dict: dictionary with image paths as keys and labels as values"""
@@ -48,17 +46,18 @@ def get_data_loader(image_dict : dict[str, int],
         transforms.ToTensor(),
     ])
 
-    if augment:
+    if augment is True:
         transform = transforms.Compose([
-            transforms.Resize((512, 512)),
+            transforms.RandomResizedCrop(512, scale=(0.8, 1.0), ratio=(0.9, 1.1)),
             transforms.RandomHorizontalFlip(),
+            transforms.RandomApply([transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.02)], p=0.5),
             transforms.RandomRotation(10),
             transforms.ToTensor(),
         ])
     
-    if custom_transforms:
-        transform = custom_transforms
-    
+    if augment and isinstance(augment, transforms.Compose):
+        transform = augment
+
     dataset = CustomDataset(image_dict=image_dict, transform=transform)
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     
