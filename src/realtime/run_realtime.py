5ec8f35c07334e17e8ee45e30fa1701ce46a137b
vozamhcakk <vozamhcak@gmail.com>
2025-11-17 03:57:15 +0300

    added realtime inference

---
+++ src/realtime/run_realtime.py
@@ -0,0 +1,51 @@
+import argparse
+from src.realtime.video_inference import VideoInference
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Real-time Road Accident Detection")
+
+    parser.add_argument(
+        "--video",
+        type=str,
+        default=None,
+        help="Путь к видеофайлу (например: video.mp4)"
+    )
+
+    parser.add_argument(
+        "--camera",
+        type=int,
+        default=None,
+        help="ID камеры (обычно 0)"
+    )
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="dict_models/vgg16_baseline.pth",
+        help="Путь к весам модели .pth"
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.video is None and args.camera is None:
+        print("[ERROR] Укажите источник: --video path/to/file.mp4 или --camera 0")
+        return
+
+    inference = VideoInference(model_path=args.model)
+
+    if args.video:
+        print(f"[INFO] Запуск видеофайла: {args.video}")
+        inference.run(source=args.video)
+
+    elif args.camera is not None:
+        print(f"[INFO] Запуск камеры: {args.camera}")
+        inference.run(source=args.camera)
+
+
+if __name__ == "__main__":
+    main()
